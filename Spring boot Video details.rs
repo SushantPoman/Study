dce181a703ce814ec000bcc8fdd8db78967d7e9b
Sushant Poman <sushantpoman94@gmail.com>
2024-03-13 21:39:13 +0530

    questions

---
+++ Spring boot Video details.rs
@@ -0,0 +1,40 @@
+Spring boot
+
+07-35-SBMS-13-Oct-23 - Create First project ways (STS, spring initializer, command prompt), mvn, run jar/war file
+
+
+
+21-35-SBMS-07-NOV-23 - Microservice -> New UserService, greet
+
+29-35-SBMS-17-NOV-23 - Hibernate -> crate table in mysql using spring, hibernate
+30-35-SBMS-20-NOV-23 - Hibernate -> getAllUsers
+31-35-SBMS-21-NOV-23 - Hibernate -> serializable, getuserById, @Transaction
+32-35-SBMS-22-NOV-23 - Spring DataJpa -> Repository CRUD, mysql connection with Springboot DataJpa
+33-35-SBMS-23-NOV-23 - Spring DataJpa -> OrderService, Userservice Crud - fetchDataByColName(Query methods)
+34-35-SBMS-24-NOV-23 - Spring DataJpa -> Userservice Crud - Custom methods using Queries, PagingAndSorting Repository
+35-35-SBMS-27-NOV-23 - Transaction Management -> JDBCTransactionDemo
+36-35-SBMS-29-NOV-23 - Spring cloud -> Microservice communication(RestTemplate)
+37-35-SBMS-30-NOV-23 - Spring cloud -> Swagger documentation
+38-35-SBMS-01-DEC-23 - Spring cloud -> Eureka server(Internal MS communication), Horizontal load balancing, 
+39-35-SBMS-04-DEC-23 - Spring cloud -> Api gateway(External MS communication)
+40-35-SBMS-05-DEC-23 - Spring cloud -> Actuator, Spring boot admin(graphical view)
+41-35-SBMS-06-DEC-23 - Spring cloud -> Sleuth(Tracing), zipkin(Tracing - graphical view)
+42-35-SBMS-08-DEC-23 - Spring cloud -> Circuit Breaker - Resilience4j
+43-35-SBMS-11-DEC-23 - Spring security -> WebMvc, Default authentication, Spring security default generated pwd for html
+44-35-SBMS-12-DEC-23 - Spring security -> Retrieve pwd from DB (Authentication using DB) using BcryptEncoder
+45-35-SBMS-13-DEC-23 - Spring security -> Custom login/error/logout form
+46-35-SBMS-14-DEC-23 - Spring security -> OAuth, JWT Token passing as request headers, internal jwt working
+47-35-SBMS-15-DEC-23 - Deployment -> Docker
+48-35-SBMS-16-DEC-23 - Keycloak
+
+
+
+
+ 
+
+
+
+
+
+
+
